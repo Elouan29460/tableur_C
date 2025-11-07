dc1238348c5a205749fc0998f99f499c83b23bcb
Elouan <122803180+Elouan29460@users.noreply.github.com>
2025-11-07 17:28:31 +0100

    Add list management functions in liste.c

--- liste.c
+++ liste.c
@@ -1,27 +1,42 @@
 #include <stdlib.h>
 #include "liste.h"
 
-
+/**
+ * Creation d'une nouvelle liste
+ */
 node_t * liste_create(void) {
     return NULL;
 }
 
+/**
+ * Renvoie le premier élément de la liste
+ */
 void * list_get_data(const node_t * node) {
     if (!node) return NULL;
     return node->val;
 }
 
+/**
+ * Lire ou écrire la donnée d'un noeud
+ */
 void list_set_data(node_t * node , void * data) {
     if (node) {
         node->val = data;
     }
 }
 
+/**
+ * Obtenir le noeud suivant
+ */
 node_t * list_next(node_t * node) {
     if (!node) return NULL;
     return node->next;
 }
 
+/**
+ * Creation et insertion d'un noeud en tête de liste
+ * Retourne la tête de la liste
+ */
 node_t * list_insert(node_t * head, void * data) {
     node_t* node = malloc(sizeof(node_t));
     if (!node) return head;
@@ -30,6 +45,10 @@ node_t * list_insert(node_t * head, void * data) {
     return node;
 }
 
+/**
+ * Creation et ajout d'un noeud en tête de liste
+ * Retourne la tête de la liste
+ */
 node_t * list_append(node_t * head, void * data) {
     node_t * node = malloc(sizeof(node_t));
     if (!node || !data) return NULL;
@@ -46,6 +65,10 @@ node_t * list_append(node_t * head, void * data) {
     return head;
 }
 
+/**
+ * Suppresion de la première instance d'une donnée dans la liste
+ * Retourne la tete de liste
+ */
 node_t * list_remove(node_t * head, void * data) {
     if (!head) return NULL;
 
@@ -71,6 +94,10 @@ node_t * list_remove(node_t * head, void * data) {
     return head;
 }
 
+/**
+ * Suppression de la tête de la liste
+ * Retourne la nouvelle tête de liste
+ */
 node_t * list_headRemove(node_t * head) {
     if (!head) return NULL; 
 
@@ -79,6 +106,10 @@ node_t * list_headRemove(node_t * head) {
     return next;
 }
 
+/**
+ * destruction d'une liste
+ * (La libération des données n'est pas prise en charge)
+ */
 void list_destroy(node_t * head) {
     node_t * curr = head;
     while (curr) {
@@ -88,6 +119,9 @@ void list_destroy(node_t * head) {
     }
 }
 
+/**
+ * Affichage des données dans une liste
+ */
 void list_print(node_t * head, void (*fct)(void*)) {
     node_t * ptr = head;
     while (ptr) {
@@ -95,4 +129,3 @@ void list_print(node_t * head, void (*fct)(void*)) {
         ptr = list_next(ptr);
     }
 }
-
