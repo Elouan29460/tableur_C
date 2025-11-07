3db57a485ea0225655c4ac9fc99051b5eafb8765
Elouan <122803180+Elouan29460@users.noreply.github.com>
2025-11-07 15:32:05 +0100

    Update liste.c

--- liste.c
+++ liste.c
@@ -1,1 +1,32 @@
+#include <stdlib.h>
+#include "liste.h"
+
+
+node_t * liste_create(void) {
+    return NULL;
+}
+
+void * list_get_data(const node_t * node) {
+    if (node == NULL) {
+        perror("erreur");
+    }
+    return node->val;
+}
+
+void list_set_data(node_t * node , void * data) {
+    node->val = data;
+}
+
+node_t * list_next(node_t * node) {
+    return node->next;
+}
+
+node_t * list_insert(node_t * head, void * data) {
+    node_t * node = liste_create();
+    head = malloc(sizeof(node_t*));
+    node->val = data;
+    node->next = head;
+    return node;
+}
+
 
