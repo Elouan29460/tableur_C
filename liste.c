9db22b971ea6b261fc437f918cb89b97a5efa2d8
Elouan <122803180+Elouan29460@users.noreply.github.com>
2025-11-07 16:36:42 +0100

    Add functions to remove nodes from the list
    Implement list_remove function to remove a node by value and list_headRemove to remove the head node.

--- liste.c
+++ liste.c
@@ -47,6 +47,35 @@ node_t * list_append(node_t * head, void * data) {
 }
 
 node_t * list_remove(node_t * head, void * data) {
+    if (!head) return NULL;
+
+    if (head->val == data) {
+        node_t * next = head->next;
+        free(head);
+        return next;
+    }
+
+    node_t* prev = head;
+    node_t* curr = head->next;
+
+    while (curr) {
+        if (curr->val == data) {
+            prev->next = curr->next;
+            free(curr);
+            break;
+        }
+        prev = curr;
+        curr = curr->next;
+    }
+
+    return head;
+}
+
+node_t * list_headRemove(node_t * head) {
+    if (!head) return NULL; 
+
+    node_t* next = head->next;
+    free(head);
     
 }
 
